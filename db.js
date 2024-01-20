946a089bf9bfaade8d89c81a2f0825053f765a71
Andy Vo <andyvo190998@gmail.com>
2024-01-20 19:07:49 +0100

    add process.env

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2'
 import dotenv from 'dotenv'
 
 const ENV = dotenv.config().parsed
-console.log(ENV.DB_USER)
+console.log(process.env.DB_PW)
 export const db = mysql.createConnection({
     host: "database-1.c36g2uc06w0l.eu-central-1.rds.amazonaws.com",
     user: 'admin',
